e08238ec431bfb26d7a87932e30dfd9bdc49ea2d
jli6535 <86691138+jli6535@users.noreply.github.com>
2021-07-01 12:32:53 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ input.onButtonPressed(Button.A, function () {
             . . . . .
             # # # # #
             `)
-        for (let index = 0; index < 25; index++) {
+        for (let index2 = 0; index2 < 25; index2++) {
             pins.digitalWritePin(DigitalPin.P0, 1)
             control.waitMicros(50000)
             pins.digitalWritePin(DigitalPin.P0, 0)
@@ -25,6 +25,10 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
+input.onButtonPressed(Button.B, function () {
+	
+})
+let index = 0
 basic.showLeds(`
     . . . . .
     . . . . #
@@ -33,6 +37,42 @@ basic.showLeds(`
     . # . . .
     `)
 pins.digitalWritePin(DigitalPin.P0, 1)
+control.waitMicros(50000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+if (index == 0) {
+    while (index == 0) {
+        for (let xindex = 0; xindex <= 4; xindex++) {
+            led.plot(xindex, 0)
+            led.plot(xindex, 1)
+            led.plot(xindex, 2)
+            led.plot(xindex, 3)
+            led.plot(xindex, 4)
+            basic.pause(100)
+            led.unplot(xindex, 0)
+            led.unplot(xindex, 1)
+            led.unplot(xindex, 2)
+            led.unplot(xindex, 3)
+            led.unplot(xindex, 4)
+        }
+    }
+} else {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+basic.forever(function () {
+	
+})
 basic.forever(function () {
 	
 })
